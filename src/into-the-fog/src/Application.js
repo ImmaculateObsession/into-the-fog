ce5bf3fc4759a236430e4c38ccd222101d7fe0f4
Philip James <pjj@philipjohnjames.com>
2013-05-06 23:07:03 -0700

    Added basic views to try and get parallax working.

--- src/into-the-fog/src/Application.js
+++ src/into-the-fog/src/Application.js
@@ -1,15 +1,36 @@
+import ui.ImageScaleView as ImageScaleView;
+import ui.ImageView as ImageView;
 import ui.TextView as TextView;
+import src.lib.platformer.ParallaxView as ParallaxView;
 
 exports = Class(GC.Application, function () {
 
 	this.initUI = function () {
-		var textview = new TextView({
+		var parallaxView = new ParallaxView({
 			superview: this.view,
-			layout: "box",
-			text: "Hello, world!",
-			color: "white"
+			width: this.view.style.width,
+			height: this.style.height,
+		});
+		parallaxView.addBackgroundView(new ImageScaleView({
+			scaleMethod: 'cover',
+			image: "resources/images/level/backgroundSky.png",
+		}));
+		var brushLayer = parallaxView.addLayer({
+			distance: 20,
+			populate: function (layer, x) {
+				var v = layer.obtainView(ImageView, {
+					superview: layer,
+					image: "resources/images/level/fargroundBrush.png",
+					x: x,
+					y: layer.style.height - 250,
+					opacity: 0.5,
+					width: 1024,
+					height: 212,
+				});
+				return v.style.width;
+			}
 		});
 	};
-	
+
 	this.launchUI = function () {};
 });
